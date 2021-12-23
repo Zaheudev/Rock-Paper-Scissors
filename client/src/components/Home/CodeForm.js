02c979ae1d4f57c9c01c0fbbb2ad4659ceba2bf1
Zaheudev <68231115+Zaheudev@users.noreply.github.com>
2021-12-23 20:39:40 +0200

    Finshed an innput style

--- client/src/components/Home/CodeForm.js
+++ client/src/components/Home/CodeForm.js
@@ -3,7 +3,7 @@ import React from "react";
 const CodeForm = () => {
   return (
     <form id="codeForm">
-      <input type="number" placeholder="Code" id="code" />
+      <input type="number" placeholder="Code" max={"4"} id="code" />
     </form>
   );
 };
